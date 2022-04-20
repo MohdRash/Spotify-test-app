360170edb09ad6183cb1a68e2b6cf87e6888b26e
Mohammed Rasheek M <mrshk.osperb@gmail.com>
2022-04-20 02:29:25 -0700

    v1: smart contract connections done

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import { MoralisProvider } from "react-moralis";
 
+//Here you have to enter your appId and serverUrl in the "xxxx" from remix editor
 
 ReactDOM.render(
   <React.StrictMode>
